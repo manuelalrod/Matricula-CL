c9e80a26e4c1c959dfbd582483e4cfd5944e3eb0
manuelalrod <manuelalmengor32@gmail.com>
2024-06-30 17:29:35 -0500

    adding pages and the route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CursoComponent } from './pages/administrador/curso/curso.component';
+import { ProfesorComponent } from './pages/administrador/profesor/profesor.component';
+import { SolicitudesComponent } from './pages/administrador/solicitudes/solicitudes.component';
+import { HorarioComponent } from './pages/estudiante/horario/horario.component';
+import { IniciarComponent } from './pages/estudiante/iniciar/iniciar.component';
+import { InicioComponent } from './pages/Inicio/inicio.component';
+import { IniciarPComponent } from './pages/profesor/iniciar-p/iniciar-p.component';
+import { HorarioPComponent } from './pages/profesor/horario-p/horario-p.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    CursoComponent, 
+    ProfesorComponent, 
+    SolicitudesComponent, 
+    IniciarComponent, 
+    HorarioComponent,
+    InicioComponent,
+    IniciarPComponent,
+    HorarioPComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/estudiante/solicitar/solicitar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitar',
+  standalone: true,
+  imports: [],
+  templateUrl: './solicitar.component.html',
+  styleUrl: './solicitar.component.css'
+})
+export class SolicitarComponent {
+
+}
