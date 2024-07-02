7e039c868731ecc14df58fadc706a870d82867e3
manuelalrod <manuelalmengor32@gmail.com>
2024-07-02 01:35:40 -0500

    añadiendo horario

--- src/app/pages/administrador/horarios/horarios.component.ts
+++ src/app/pages/administrador/horarios/horarios.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HorarioService } from '../../../shared/horario/horario.service';
+import { CursoService } from '../../../shared/curso/curso.service';
+import { ProfesorService } from '../../../shared/profesor/profesor.service';
+import { CursoModel } from '../../../shared/curso/curso.model';
+import { ProfesorModel } from '../../../shared/profesor/profesor.model';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-horarios',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './horarios.component.html',
   styleUrl: './horarios.component.css'
 })
-export class HorariosComponent {
+export class HorariosComponent implements OnInit {
 
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
