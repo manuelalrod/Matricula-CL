d2746cc8646be2348a058d60ff30e0411d38fd9a
Thana1329 <aythanarios133@gmail.com>
2024-07-02 01:28:32 -0500

    Añadiendo el diseño

--- src/app/pages/administrador/horarios/horarios.component.ts
+++ src/app/pages/administrador/horarios/horarios.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-horarios',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './horarios.component.html',
   styleUrl: './horarios.component.css'
 })
--- src/app/pages/administrador/solicitudes/solicitudes.component.ts
+++ src/app/pages/administrador/solicitudes/solicitudes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-solicitudes',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './solicitudes.component.html',
   styleUrl: './solicitudes.component.css'
 })
