6e63e99ad7e5eff48c477e1b0104d8cb101d20db
manuelalrod <manuelalmengor32@gmail.com>
2024-07-03 01:31:11 -0500

    create iniciar

---
+++ src/app/pages/administrador/iniciar-a/iniciar-a.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-iniciar-a',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './iniciar-a.component.html',
+  styleUrl: './iniciar-a.component.css'
+})
+export class IniciarAComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.username === 'Admin' && this.password === '12345') {
+      
+      this.router.navigate(['/curso']);
+    } else {
+      this.errorMessage = 'Invalid username or password.';
+    }
+  }
+}
